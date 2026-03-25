09f2392cd7f475097c77888a10564de562b5e49d
xPepp3z <giuseppegravagno57@gmail.com>
2026-03-25 11:31:44 +0100

    chore: stop tracking env and build artifacts

--- dist/auth/register.d.ts
+++
@@ -1,3 +0,0 @@
-import express, { type Request, type Response } from 'express';
-export declare const userRegister: (req: Request, res: Response) => Promise<express.Response<any, Record<string, any>>>;
-//# sourceMappingURL=register.d.ts.map
--- dist/controllers/orderController.d.ts
+++
@@ -1,7 +0,0 @@
-import express, { type Request, type Response } from 'express';
-export declare const getOrders: (req: Request, res: Response) => express.Response<any, Record<string, any>> | undefined;
-export declare const newOrder: (req: Request, res: Response) => Promise<void>;
-export declare const updateOrder: (req: Request, res: Response) => Promise<express.Response<any, Record<string, any>>>;
-export declare const deleteOrder: (req: Request, res: Response) => Promise<express.Response<any, Record<string, any>>>;
-export declare const getMyOrder: (req: Request, res: Response) => express.Response<any, Record<string, any>>;
-//# sourceMappingURL=orderController.d.ts.map
